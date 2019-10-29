b53d6f4f09b7765eb8e7dddaee38b8ff69c32e80
JiangKan <18369564020@163.com>
2019-10-29 11:17:52 +0800

    Modified contract about vankia.hblf

--- contracts/vankia.hblf/vankia.hblf.cpp
+++ contracts/vankia.hblf/vankia.hblf.cpp
@@ -1,47 +1,306 @@
 #include <eosio.token/eosio.token.hpp>
 #include <eosio.system/native.hpp>
 #include "vankia.hblf.hpp"
+
 namespace vankia
 {
-using namespace eosio;
-using namespace std;
-using namespace eosiosystem;
-/// @abi action 
-void hblf::addstudent(string studentCode, string userId, string studentname, string status, vector<keypair> contentmap)
-{
+    using namespace eosio;
+    using namespace std;
+    using namespace eosiosystem;
+
+    /// @abi action
+    void hblf::addsecooofir(string tzjkid,string xxdm,string xxmc,string xsid,string xm,string nj,string njmc,string xjh,string xnm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::modsecooofir(string tzjkid,string xxdm,string xxmc,string xsid,string xm,string nj,string njmc,string xjh,string xnm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::delsecooofir(string tzjkid,string xxdm,string xxmc,string xsid,string xm,string nj,string njmc,string xjh,string xnm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::addsecooosec(string rwid,string xjh,string xsid,string xxdm,string yhid,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::modsecooosec(string rwid,string xjh,string xsid,string xxdm,string yhid,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::delsecooosec(string rwid,string xjh,string xsid,string xxdm,string yhid,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::addeigooofir(string jsid,string jsxm,string rybh,string rymc,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::modeigooofir(string jsid,string jsxm,string rybh,string rymc,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::deleigooofir(string jsid,string jsxm,string rybh,string rymc,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::addfifooofir(string jsid,string zcbh,string zcmc,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::modfifooofir(string jsid,string zcbh,string zcmc,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::delfifooofir(string jsid,string zcbh,string zcmc,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::addfifooosec(string jsid,string hdxlbh,string hdlxmc,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::modfifooosec(string jsid,string hdxlbh,string hdlxmc,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::delfifooosec(string jsid,string hdxlbh,string hdlxmc,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::addfifooothi(string jsid,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::modfifooothi(string jsid,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::delfifooothi(string jsid,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::addfifooofou(string jsid,string zzmmbh,string zzmm,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::modfifooofou(string jsid,string zzmmbh,string zzmm,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::delfifooofou(string jsid,string zzmmbh,string zzmm,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::addfifooofif(string jsid,string prdwbh,string prdw,string gzdwbh,string gzdw,string przw,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::modfifooofif(string jsid,string prdwbh,string prdw,string gzdwbh,string gzdw,string przw,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::delfifooofif(string jsid,string prdwbh,string prdw,string gzdwbh,string gzdw,string przw,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::addfifooosix(string jsid,string yzbh,string yz,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::modfifooosix(string jsid,string yzbh,string yz,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::delfifooosix(string jsid,string yzbh,string yz,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::addfifooosev(string jsid,string jszzzzlbh,string jszzzzl,string jszzzhm,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::modfifooosev(string jsid,string jszzzzlbh,string jszzzzl,string jszzzhm,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::delfifooosev(string jsid,string jszzzzlbh,string jszzzzl,string jszzzhm,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::addfifoooeig(string jsid,string jnlxbh,string jnlx,string jnmc,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::modfifoooeig(string jsid,string jnlxbh,string jnlx,string jnmc,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::delfifoooeig(string jsid,string jnlxbh,string jnlx,string jnmc,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::addzerooofir(string orgId,string orgCode,string orgType,string orgName,string areaId,string areaName,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::modzerooofir(string orgId,string orgCode,string orgType,string orgName,string areaId,string areaName,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::delzerooofir(string orgId,string orgCode,string orgType,string orgName,string areaId,string areaName,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::addzerooosev(string orgId,string schoolName,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::modzerooosev(string orgId,string schoolName,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::delzerooosev(string orgId,string schoolName,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::addzerooofif(string userId,string studentCode,string stuName,string orgId,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::modzerooofif(string userId,string studentCode,string stuName,string orgId,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::delzerooofif(string userId,string studentCode,string stuName,string orgId,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::addzerooosix(string orgId,string userId,string teaName,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::modzerooosix(string orgId,string userId,string teaName,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::delzerooosix(string orgId,string userId,string teaName,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::addfouooothi(string xscjid,string xxdm,string ksid,string xsid,string xm,string kmm,string kmmc,string fslkscj,string djlkscj,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::modfouooothi(string xscjid,string xxdm,string ksid,string xsid,string xm,string kmm,string kmmc,string fslkscj,string djlkscj,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::delfouooothi(string xscjid,string xxdm,string ksid,string xsid,string xm,string kmm,string kmmc,string fslkscj,string djlkscj,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::addthiooofir(string jsid,string jsxm,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::modthiooofir(string jsid,string jsxm,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::delthiooofir(string jsid,string jsxm,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::addthiooosec(string jsid,string jsxm,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::modthiooosec(string jsid,string jsxm,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::delthiooosec(string jsid,string jsxm,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::addthiooothi(string jsid,string jsxm,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::modthiooothi(string jsid,string jsxm,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::delthiooothi(string jsid,string jsxm,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::addthiooofou(string jsid,string jsxm,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::modthiooofou(string jsid,string jsxm,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::delthiooofou(string jsid,string jsxm,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::addfouooofir(string xxdm,string ksid,string ksmc,string yhid,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::modfouooofir(string xxdm,string ksid,string ksmc,string yhid,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::delfouooofir(string xxdm,string ksid,string ksmc,string yhid,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::addfouooosec(string xxdm,string ksid,string ksmc,string kmm,string kmmc,string sjid,string yhid,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::modfouooosec(string xxdm,string ksid,string ksmc,string kmm,string kmmc,string sjid,string yhid,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::delfouooosec(string xxdm,string ksid,string ksmc,string kmm,string kmmc,string sjid,string yhid,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::addfouooofou(string xxdm,string sjid,string yhid,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::modfouooofou(string xxdm,string sjid,string yhid,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::delfouooofou(string xxdm,string sjid,string yhid,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::addfouooofif(string xxdm,string zsdid,string stid,string yhid,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::modfouooofif(string xxdm,string zsdid,string stid,string yhid,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::delfouooofif(string xxdm,string zsdid,string stid,string yhid,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::addfouooosix(string xxdm,string ksid,string ksmc,string xscjid,string sxcjmxid,string sjid,string sjdf,string yhid,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::modfouooosix(string xxdm,string ksid,string ksmc,string xscjid,string sxcjmxid,string sjid,string sjdf,string yhid,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::delfouooosix(string xxdm,string ksid,string ksmc,string xscjid,string sxcjmxid,string sjid,string sjdf,string yhid,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::addeigooosec(string jsid,string jsxm,string qqlxbh,string qqlx,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::modeigooosec(string jsid,string jsxm,string qqlxbh,string qqlx,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::deleigooosec(string jsid,string jsxm,string qqlxbh,string qqlx,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::addninooofir(string jsid,string yzkbh,string yzkmc,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::modninooofir(string jsid,string yzkbh,string yzkmc,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::delninooofir(string jsid,string yzkbh,string yzkmc,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::addninooosec(string jsid,string zlh,string zlmc,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::modninooosec(string jsid,string zlh,string zlmc,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::delninooosec(string jsid,string zlh,string zlmc,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::addninooothi(string jsid,string ktbh,string ktmc,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::modninooothi(string jsid,string ktbh,string ktmc,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::delninooothi(string jsid,string ktbh,string ktmc,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::addninooofou(string jsid,string zzbh,string zzmc,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::modninooofou(string jsid,string zzbh,string zzmc,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::delninooofou(string jsid,string zzbh,string zzmc,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::addninooofif(string jsid,string lwbh,string lwmc,string lwlxbh,string lwlx,string xxdm,string sfyx){}
+
+    /// @abi action
+    void hblf::modninooofif(string jsid,string lwbh,string lwmc,string lwlxbh,string lwlx,string xxdm,string sfyx){}
+
+    /// @abi action
+    void hblf::delninooofif(string jsid,string lwbh,string lwmc,string lwlxbh,string lwlx,string xxdm,string sfyx){}
+
+
+
+
+
 
 }
 
-/// @abi action 
-void hblf::modstudent(string studentCode, string userId, string studentname, string status, vector<keypair> contentmap)
-{
 
-}
+// namespace vankia
 
-/// @abi action 
-void hblf::delstudent(string studentCode, string userId, string studentname, string status, vector<keypair> contentmap)
-{
+EOSIO_DISPATCH(vankia::hblf,(addsecooofir)(modsecooofir)(delsecooofir)(addsecooosec)(modsecooosec)(delsecooosec)(addeigooofir)(modeigooofir)(deleigooofir)(addfifooofir)(modfifooofir)(delfifooofir)(addfifooosec)(modfifooosec)(delfifooosec)(addfifooothi)(modfifooothi)(delfifooothi)(addfifooofou)(modfifooofou)(delfifooofou)(addfifooofif)(modfifooofif)(delfifooofif)(addfifooosix)(modfifooosix)(delfifooosix)(addfifooosev)(modfifooosev)
+(delfifooosev)(addfifoooeig)(modfifoooeig)(delfifoooeig)(addzerooofir)(modzerooofir)(delzerooofir)(addzerooosev)(modzerooosev)(delzerooosev)(addzerooofif)(modzerooofif)(delzerooofif)(addzerooosix)(modzerooosix)(delzerooosix)(addfouooothi)(modfouooothi)(delfouooothi)(addthiooofir)(modthiooofir)(delthiooofir)(addthiooosec)(modthiooosec)(delthiooosec)(addthiooothi)(modthiooothi)(delthiooothi)(addthiooofou)(modthiooofou)(delthiooofou) \
+(addfouooofir)(modfouooofir)(delfouooofir)(addfouooosec)(modfouooosec)(delfouooosec)(addfouooofou)(modfouooofou)(delfouooofou)(addfouooofif)(modfouooofif)(delfouooofif)(addfouooosix)(modfouooosix)(delfouooosix)(addeigooosec)(modeigooosec)(deleigooosec)(addninooofir)(modninooofir)(delninooofir)(addninooosec)(modninooosec)(delninooosec)(addninooothi)(modninooothi)(delninooothi)(addninooofou)(modninooofou)(delninooofou)(addninooofif)(modninooofif)(delninooofif)   \
 
-}
 
-/// @abi action 
-void hblf::addteachbase(string teachCode, string userId, string teachname, string status, vector<keypair> contentmap)
-{
 
-}
-
-/// @abi action 
-void hblf::modteachbase(string teachCode, string userId, string teachname, string status, vector<keypair> contentmap)
-{
-
-}
-
-/// @abi action 
-void hblf::delteachbase(string teachCode, string userId, string teachname, string status, vector<keypair> contentmap)
-{
-
-}
-
-} // namespace vankia
-
-EOSIO_DISPATCH(vankia::hblf, (addstudent)(modstudent)(delstudent)(addteachbase)(modteachbase)(delteachbase))
+)
