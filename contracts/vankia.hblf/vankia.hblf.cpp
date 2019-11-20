08f1a18330d8e07eec0e244d44d51cec6984f150
JiangKan <18369564020@163.com>
2019-11-20 17:53:28 +0800

    vankia.hblf.cpp

--- contracts/vankia.hblf/vankia.hblf.cpp
+++ contracts/vankia.hblf/vankia.hblf.cpp
@@ -279,13 +279,252 @@ namespace vankia
     void hblf::delninooofou(string jsid,string zzbh,string zzmc,string xxdm,string sfyx,vector <keypair> contentmap){}
 
     /// @abi action
-    void hblf::addninooofif(string jsid,string lwbh,string lwmc,string lwlxbh,string lwlx,string xxdm,string sfyx){}
+    void hblf::addninooofif(string jsid,string lwbh,string lwmc,string lwlxbh,string lwlx,string xxdm,string sfyx,vector <keypair> contentmap){}
 
     /// @abi action
-    void hblf::modninooofif(string jsid,string lwbh,string lwmc,string lwlxbh,string lwlx,string xxdm,string sfyx){}
+    void hblf::modninooofif(string jsid,string lwbh,string lwmc,string lwlxbh,string lwlx,string xxdm,string sfyx,vector <keypair> contentmap){}
 
     /// @abi action
-    void hblf::delninooofif(string jsid,string lwbh,string lwmc,string lwlxbh,string lwlx,string xxdm,string sfyx){}
+    void hblf::delninooofif(string jsid,string lwbh,string lwmc,string lwlxbh,string lwlx,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::addsixooofir(string jsid,string ryid,string rymc,string fddxid,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::modsixooofir(string jsid,string ryid,string rymc,string fddxid,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::delsixooofir(string jsid,string ryid,string rymc,string fddxid,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::addsixooosec(string jsid,string rxnfid,string rxnfmc,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::modsixooosec(string jsid,string rxnfid,string rxnfmc,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::delsixooosec(string jsid,string rxnfid,string rxnfmc,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::addsixooothi(string jsid,string jsxm,string qnjsid,string qnjsname,string rymc,string xxdm,string sfyx,vector<keypair> contentmap){}
+
+    /// @abi action
+    void hblf::modsixooothi(string jsid,string jsxm,string qnjsid,string qnjsname,string rymc,string xxdm,string sfyx,vector<keypair> contentmap){}
+
+    /// @abi action
+    void hblf::delsixooothi(string jsid,string jsxm,string qnjsid,string qnjsname,string rymc,string xxdm,string sfyx,vector<keypair> contentmap){}
+
+    /// @abi action
+    void hblf::addsevooofir(string jsid,string jsxm,string jxbh,string jxmc,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::modsevooofir(string jsid,string jsxm,string jxbh,string jxmc,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::delsevooofir(string jsid,string jsxm,string jxbh,string jxmc,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::addsevooosec(string jsid,string jsxm,string pxxmbh,string pxxm,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::modsevooosec(string jsid,string jsxm,string pxxmbh,string pxxm,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::delsevooosec(string jsid,string jsxm,string pxxmbh,string pxxm,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::addsevooothi(string jsid,string jsxm,string pxxmbh,string pxxm,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::modsevooothi(string jsid,string jsxm,string pxxmbh,string pxxm,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::delsevooothi(string jsid,string jsxm,string pxxmbh,string pxxm,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::addsevooofou(string jsid,string jsxm,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::modsevooofou(string jsid,string jsxm,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::delsevooofou(string jsid,string jsxm,string xxdm,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::addzerooosec(string teachClassId,string classId,string name,string orgId,string orgName,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::modzerooosec(string teachClassId,string classId,string name,string orgId,string orgName,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::delzerooosec(string teachClassId,string classId,string name,string orgId,string orgName,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::addzerooothi(string teachClassId,string classId,string name,string orgId,string orgName,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::modzerooothi(string teachClassId,string classId,string name,string orgId,string orgName,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::delzerooothi(string teachClassId,string classId,string name,string orgId,string orgName,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::addzerooofou(string userId,string teacherName,string teachId,string classId,string className,string orgId,string orgName,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::modzerooofou(string userId,string teacherName,string teachId,string classId,string className,string orgId,string orgName,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::delzerooofou(string userId,string teacherName,string teachId,string classId,string className,string orgId,string orgName,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::addfoufirfir(string xsid,string xqm,string xqmc,string hdmc,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::modfoufirfir(string xsid,string xqm,string xqmc,string hdmc,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::delfoufirfir(string xsid,string xqm,string xqmc,string hdmc,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::addfoufirsec(string xsid,string xqm,string xqmc,string hdmc,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::modfoufirsec(string xsid,string xqm,string xqmc,string hdmc,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::delfoufirsec(string xsid,string xqm,string xqmc,string hdmc,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::addfoufirthi(string xsid,string xqm,string xqmc,string hdmc,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::modfoufirthi(string xsid,string xqm,string xqmc,string hdmc,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::delfoufirthi(string xsid,string xqm,string xqmc,string hdmc,string sfyx,vector <keypair> contentmap){}
+
+
+
+
+    /// @abi action
+    void hblf::addresource(string resource_id,string name,string teacher_id,string teacher_name,string time,string md5,string score){}
+    /// @abi action
+    void hblf::addrscrecord(string resource_record_id,string resource_id,string name,string user_id,string user_name,string time,string score){}
+    /// @abi action
+    void hblf::addscrrecord(string record_id,string type,string value,string user_id,string user_name,string time,string remark){}
+    /// @abi action
+    void hblf::addresources(string resource_id,string name,string teacher_id,string teacher_name,string time,string md5){}
+    /// @abi action
+    void hblf::addresrecord(string resource_record_id,string resource_id,string name,string user_id,string user_name,string time){}
+    /// @abi action
+    void hblf::addtestques(string test_question_id,string question_code,string question_title,uint32_t score,string level,string analysis,string teacher_id,string teacher_name,string time,vector <option> options){}
+    /// @abi action
+    void hblf::addanswerrec(string record_id,string level_code,string level_id,string knowledge_code,string knowledge_id,string time,string student_id,string student_name){}
+    /// @abi action
+    void hblf::addansquestn(vector <aserandquest> answer_test_questions){}
+
+
+
+
+    /// @abi action
+    void hblf::addfirfirfir(string yhid,string xxbh,string xxmc,string ksbh,string ksmc,string orgid,string sfyx,vector <keypair> contentmap){}
+    /// @abi action
+    void hblf::modfirfirfir(string yhid,string xxbh,string xxmc,string ksbh,string ksmc,string orgid,string sfyx,vector<keypair> contentmap){}
+    /// @abi action
+    void hblf::delfirfirfir(string yhid,string xxbh,string xxmc,string ksbh,string ksmc,string orgid,string sfyx,vector<keypair> contentmap){}
+
+    /// @abi action
+    void hblf::addfirfirsec(string yhid,string bjbh,string bjmc,string ksbh,string ksmc,string njbh,string njmc,string kmbh,string kmmc,string xxbh,string xxmc,string orgid,vector <keypair> contentmap){}
+    /// @abi action
+    void hblf::modfirfirsec(string yhid,string bjbh,string bjmc,string ksbh,string ksmc,string njbh,string njmc,string kmbh,string kmmc,string xxbh,string xxmc,string orgid,vector <keypair> contentmap){}
+    /// @abi action
+    void hblf::delfirfirsec(string yhid,string bjbh,string bjmc,string ksbh,string ksmc,string njbh,string njmc,string kmbh,string kmmc,string xxbh,string xxmc,string orgid,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::addfirfirthi(string orgid,string jgmc,string jglx,string jgzt,string sfyx,vector <keypair> contentmap){}
+    /// @abi action
+    void hblf::modfirfirthi(string orgid,string jgmc,string jglx,string jgzt,string sfyx,vector <keypair> contentmap){}
+    /// @abi action
+    void hblf::delfirfirthi(string orgid,string jgmc,string jglx,string jgzt,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::addfirfirfou(string bjmc,string xxbh,string xxmc,string orgid,string sfyx,vector <keypair> contentmap){}
+    /// @abi action
+    void hblf::modfirfirfou(string bjmc,string xxbh,string xxmc,string orgid,string sfyx,vector <keypair> contentmap){}
+    /// @abi action
+    void hblf::delfirfirfou(string bjmc,string xxbh,string xxmc,string orgid,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::addfirfirfif(string xsbh,string xsxm,string xxbh,string xxmc,string kmbh,string kmmc,string zf,string orgid,vector <keypair> contentmap){}
+    /// @abi action
+    void hblf::modfirfirfif(string xsbh,string xsxm,string xxbh,string xxmc,string kmbh,string kmmc,string zf,string orgid,vector <keypair> contentmap){}
+    /// @abi action
+    void hblf::delfirfirfif(string xsbh,string xsxm,string xxbh,string xxmc,string kmbh,string kmmc,string zf,string orgid,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::addfirfirsix(string xsbh,string xsxm,string kmbh,string kmmc,string df,string orgid,string sfyx,vector <keypair> contentmap){}
+    /// @abi action
+    void hblf::modfirfirsix(string xsbh,string xsxm,string kmbh,string kmmc,string df,string orgid,string sfyx,vector <keypair> contentmap){}
+    /// @abi action
+    void hblf::delfirfirsix(string xsbh,string xsxm,string kmbh,string kmmc,string df,string orgid,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::addfirfirsev(string orgid,string qybh,string qymc,string xnbh,string xnmc,string ywxysp,string sxxysp,string kxxysp,string tyxysp,string ysxysp,string dyxysp,string sfyx,vector <keypair> contentmap){}
+    /// @abi action
+    void hblf::modfirfirsev(string orgid,string qybh,string qymc,string xnbh,string xnmc,string ywxysp,string sxxysp,string kxxysp,string tyxysp,string ysxysp,string dyxysp,string sfyx,vector <keypair> contentmap){}
+    /// @abi action
+    void hblf::delfirfirsev(string orgid,string qybh,string qymc,string xnbh,string xnmc,string ywxysp,string sxxysp,string kxxysp,string tyxysp,string ysxysp,string dyxysp,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::addfirfireig(string orgid,string qybh,string qymc,string xnbh,string xnmc,string xkbh,string xkmc,string njbh,string njmc,string pjf,string sfyx,vector <keypair> contentmap){}
+    /// @abi action
+    void hblf::modfirfireig(string orgid,string qybh,string qymc,string xnbh,string xnmc,string xkbh,string xkmc,string njbh,string njmc,string pjf,string sfyx,vector <keypair> contentmap){}
+    /// @abi action
+    void hblf::delfirfireig(string orgid,string qybh,string qymc,string xnbh,string xnmc,string xkbh,string xkmc,string njbh,string njmc,string pjf,string sfyx,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::addfirfirnin(string orgid,string qybh,string qymc,string xnbh,string xnmc,string cxbh,string cxmc,string njbh,string njmc,vector <keypair> contentmap){}
+    /// @abi action
+    void hblf::modfirfirnin(string orgid,string qybh,string qymc,string xnbh,string xnmc,string cxbh,string cxmc,string njbh,string njmc,vector <keypair> contentmap){}
+    /// @abi action
+    void hblf::delfirfirnin(string orgid,string qybh,string qymc,string xnbh,string xnmc,string cxbh,string cxmc,string njbh,string njmc,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::addfirfirten(string orgid,string qybh,string qymc,string xnbh,string xnmc,string cxbh,string cxmc,string njbh,string njmc,vector <keypair> contentmap){}
+    /// @abi action
+    void hblf::modfirfirten(string orgid,string qybh,string qymc,string xnbh,string xnmc,string cxbh,string cxmc,string njbh,string njmc,vector <keypair> contentmap){}
+    /// @abi action
+    void hblf::delfirfirten(string orgid,string qybh,string qymc,string xnbh,string xnmc,string cxbh,string cxmc,string njbh,string njmc,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::addfirfirele(string orgid,string qybh,string qymc,string xnbh,string xnmc,string cxbh,string cxmc,string njbh,string njmc,vector <keypair> contentmap){}
+    /// @abi action
+    void hblf::modfirfirele(string orgid,string qybh,string qymc,string xnbh,string xnmc,string cxbh,string cxmc,string njbh,string njmc,vector <keypair> contentmap){}
+    /// @abi action
+    void hblf::delfirfirele(string orgid,string qybh,string qymc,string xnbh,string xnmc,string cxbh,string cxmc,string njbh,string njmc,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::addfirfirtwe(string orgid,string qybh,string qymc,string xnbh,string xnmc,string cxbh,string cxmc,vector <keypair> contentmap){}
+    /// @abi action
+    void hblf::modfirfirtwe(string orgid,string qybh,string qymc,string xnbh,string xnmc,string cxbh,string cxmc,vector <keypair> contentmap){}
+    /// @abi action
+    void hblf::delfirfirtwe(string orgid,string qybh,string qymc,string xnbh,string xnmc,string cxbh,string cxmc,vector <keypair> contentmap){}
+
+    /// @abi action
+    void hblf::addbasicinfo(string stu_id,vector<tkeys> contentmap,string createPerson){}
+    /// @abi action
+    void hblf::modbasicinfo(string stu_id,vector<tkeys> contentmap,string createPerson){}
+    /// @abi action
+    void hblf::delbasicinfo(string stu_id,vector<tkeys> contentmap,string createPerson){}
+
+    /// @abi action
+    void hblf::addprgrminfo(string stu_id,vector<tkeys> contentmap,string createPerson){}
+    /// @abi action
+    void hblf::modprgrminfo(string stu_id,vector<tkeys> contentmap,string createPerson){}
+    /// @abi action
+    void hblf::delprgrminfo(string stu_id,vector <tkeys> contentmap,string createPerson){}
+
 
 
 
@@ -300,6 +539,11 @@ namespace vankia
 EOSIO_DISPATCH(vankia::hblf,(addsecooofir)(modsecooofir)(delsecooofir)(addsecooosec)(modsecooosec)(delsecooosec)(addeigooofir)(modeigooofir)(deleigooofir)(addfifooofir)(modfifooofir)(delfifooofir)(addfifooosec)(modfifooosec)(delfifooosec)(addfifooothi)(modfifooothi)(delfifooothi)(addfifooofou)(modfifooofou)(delfifooofou)(addfifooofif)(modfifooofif)(delfifooofif)(addfifooosix)(modfifooosix)(delfifooosix)(addfifooosev)(modfifooosev)
 (delfifooosev)(addfifoooeig)(modfifoooeig)(delfifoooeig)(addzerooofir)(modzerooofir)(delzerooofir)(addzerooosev)(modzerooosev)(delzerooosev)(addzerooofif)(modzerooofif)(delzerooofif)(addzerooosix)(modzerooosix)(delzerooosix)(addfouooothi)(modfouooothi)(delfouooothi)(addthiooofir)(modthiooofir)(delthiooofir)(addthiooosec)(modthiooosec)(delthiooosec)(addthiooothi)(modthiooothi)(delthiooothi)(addthiooofou)(modthiooofou)(delthiooofou) \
 (addfouooofir)(modfouooofir)(delfouooofir)(addfouooosec)(modfouooosec)(delfouooosec)(addfouooofou)(modfouooofou)(delfouooofou)(addfouooofif)(modfouooofif)(delfouooofif)(addfouooosix)(modfouooosix)(delfouooosix)(addeigooosec)(modeigooosec)(deleigooosec)(addninooofir)(modninooofir)(delninooofir)(addninooosec)(modninooosec)(delninooosec)(addninooothi)(modninooothi)(delninooothi)(addninooofou)(modninooofou)(delninooofou)(addninooofif)(modninooofif)(delninooofif)   \
+(addsixooofir)(modsixooofir)(delsixooofir)(addsixooosec)(modsixooosec)(delsixooosec)(addsixooothi)(modsixooothi)(delsixooothi)(addsevooofir)(modsevooofir)(delsevooofir)(addsevooosec)(modsevooosec)(delsevooosec)(addsevooothi)(modsevooothi)(delsevooothi)(addsevooofou)(modsevooofou)(delsevooofou)(addzerooosec)(modzerooosec)(delzerooosec)(addzerooothi)(modzerooothi)(delzerooothi)(addzerooofou)(modzerooofou)(delzerooofou)  \
+(addfoufirfir)(modfoufirfir)(delfoufirfir)(addfoufirsec)(modfoufirsec)(delfoufirsec)(addfoufirthi)(modfoufirthi)(delfoufirthi)(addresource)(addrscrecord)(addscrrecord) \
+(addfirfirfir)(modfirfirfir)(delfirfirfir)(addfirfirsec)(modfirfirsec)(delfirfirsec)(addfirfirthi)(modfirfirthi)(delfirfirthi)(addfirfirfou)(modfirfirfou)(delfirfirfou)(addfirfirfif)(modfirfirfif)(delfirfirfif) \
+(addfirfirsix)(modfirfirsix)(delfirfirsix)(addfirfirsev)(modfirfirsev)(delfirfirsev)(addfirfireig)(modfirfireig)(delfirfireig)(addfirfirnin)(modfirfirnin)(delfirfirnin)(addfirfirten)(modfirfirten)(delfirfirten) \
+(addfirfirele)(modfirfirele)(delfirfirele)(addfirfirtwe)(modfirfirtwe)(delfirfirtwe)(addresources)(addresrecord)(addtestques)(addanswerrec)(addbasicinfo)(modbasicinfo)(delbasicinfo)(addprgrminfo)(modprgrminfo)(delprgrminfo)
 
 
 
